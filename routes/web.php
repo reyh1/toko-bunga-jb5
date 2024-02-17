3ddb92e54622cc40c75ba9e1858127beefdd6820
M. Ibaneza Fendera A. <149344118+KyozuID@users.noreply.github.com>
2024-02-17 16:10:20 +0700

    Membuat crud dari laravel

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\PostController;
+use App\Http\Controllers\TambahController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
+Route::get('/tambah', [TambahController::class, 'tambah']);
